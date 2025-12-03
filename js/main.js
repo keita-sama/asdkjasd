6165ff95717eeef56c1af0a0d3b8caf4421c2580
Keita <87774086+keita-sama@users.noreply.github.com>
2025-12-03 02:15:21 +0200

    oh my gyatt please work

---
+++ js/main.js
@@ -0,0 +1,65 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      let reg;
+      reg = await navigator.serviceWorker.register("/service-worker.js");
+      console.log("Service worker registered!", reg);
+    } catch (err) {
+      console.log("Service worker registration failed: ", err);
+    }
+  });
+}
+
+function showCode() {
+  const canvas = document.getElementById("drawer");
+  const editor = document.getElementById("editor");
+
+  const codeBtn = document.getElementById("code");
+  const imageBtn = document.getElementById("image");
+
+  if (!canvas.classList.contains("off")) {
+    canvas.classList.add("off");
+    codeBtn.classList.add("btn-selected");
+    imageBtn.classList.remove("btn-selected");
+  }
+  if (editor.classList.contains("off")) {
+    editor.classList.remove("off");
+  }
+}
+
+function showDrawn() {
+  const canvas = document.getElementById("drawer");
+  const editor = document.getElementById("editor");
+
+  const codeBtn = document.getElementById("code");
+  const imageBtn = document.getElementById("image");
+
+  if (!editor.classList.contains("off")) {
+    editor.classList.add("off");
+    codeBtn.classList.remove("btn-selected");
+    imageBtn.classList.add("btn-selected");
+  }
+  if (canvas.classList.contains("off")) {
+    canvas.classList.remove("off");
+  }
+}
+
+// Ace editor stuff
+// function indentLine() {
+//     window.editor.blockIndent()
+// }
+
+// function unindentLine() {
+//     window.editor.blockOutdent()
+// }
+
+// function pasteText() {
+//     navigator.clipboard.readText().then(text => {
+//         document.getElementById('editor').innerHTML = text;
+//     })
+// }
+
+function runCode() {
+  showDrawn();
+  runit();
+}
---
+++ service-worker.js
@@ -0,0 +1,53 @@
+// Choose a cache name
+const cacheName = "cache-v1";
+// List the files to precache
+const precacheResources = [
+  // imma be real i made the extra resolutions but i don't think im gonna use them
+  "images/128.png",
+  "images/192.png",
+  "images/512.png",
+  "images/codelogo.svg",
+
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/js/main.js",
+
+  "/lib/ace.js",
+  "/lib/ace.min.js",
+  "/lib/mode-python.js",
+  "/lib/theme-tomorrow_night_bright.js",
+
+  "/lib/codicons/codicons.css",
+  "/lib/codicons/codicons.min.css",
+  "/lib/codicons/codicons.ttf",
+
+  "/lib/skulpt/skulpt-stdlib.js",
+  "/lib/skulpt/skulpt.js",
+  "/lib/skulpt/skulpt.min.js",
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener("install", (event) => {
+  console.log("Service worker install event!");
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(precacheResources))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activate event!");
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener("fetch", (event) => {
+  console.log("Fetch intercepted for:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
